b39a83ddd542419f207b1e7dd713e0cf59041921
Alex Saveliev <lyolik@codeminders.com>
2016-01-21 12:08:48 +0200

    java project with Srcfile

---
+++ src/main/java/foo/bar/baz/Qux.java
@@ -0,0 +1,10 @@
+package foo.bar.baz;
+
+import com.beust.jcommander.JCommander;
+
+public class Qux {
+	
+	public static void main(String args[]) throws Exception {
+		JCommander jc = new JCommander();
+	}
+}
